858018ea3932f6ae4c8fa111f356d2cc38a70900
Nika <129578493+nixiwqq@users.noreply.github.com>
2024-10-03 21:56:54 +0200

    Add files via upload

---
+++ homew.py
@@ -0,0 +1,10 @@
+class Colors:
+    YELLOW = "\033[1;33m"
+    RED = "\033[0;31m"
+    END = "\033[0m"
+print('\nTo be \n\033[0;31mor not \n\033[0mto be')
+
+print()
+print()
+print()
+print('\033[1;33m\"Life is what happens \n\twhen \n\t\tyou\'re busy making other plans\" \n\t\t\t\t\t\t\t\t\t\033[0mJohn Lennon')
